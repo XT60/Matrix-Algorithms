0f7a87b7f4e48b33e75797349f801fc6be6add13
aszarata <aszarata@student.agh.edu.pl>
2024-01-08 13:39:12 +0100

    lab4

--- zad4/main.py
+++ zad4/main.py
@@ -1,6 +1,6 @@
 from compress import compress_matrix, decompress_matrix
 from utils import generate_3d_matrix_mesh, sparse_draw
-from permutations import cuthill_mckee, reversed_cuthill_mckee, minimum_degree_permutation, permutate
+from permutations import PermutationEngine
 
 for k in [2, 3, 4]:
     matrix = generate_3d_matrix_mesh(k)
@@ -9,18 +9,44 @@ for k in [2, 3, 4]:
     sparse_draw(matrix, f"./zad4/data/k_{k}/2a.png")
 
     #draw matrix after compression
-    compressed_matrix = compress_matrix(matrix, max_rank=4, min_singular_val=0)
+    compressed_matrix = compress_matrix(matrix.copy(), max_rank=4, min_singular_val=0)
     compressed_matrix.draw_matrix(f"./zad4/data/k_{k}/2b.png")
     
 
-    #draw matrix after permutation
-    permutation = cuthill_mckee(matrix)
+    # Minimum-Degree
+    permutation_engine = PermutationEngine('minimum_degree')
 
-    permutated_matrix = permutate(matrix, permutation)
-    sparse_draw(permutated_matrix, f"./zad4/data/k_{k}/2c.png")
+    permutated_matrix = permutation_engine.permutate(matrix.copy())
+    sparse_draw(permutated_matrix, f"./zad4/data/k_{k}/2c-min_deg.png")
     
 
     #draw matrix after compressiona and permutation
     compressed_permutated_matrix = compress_matrix(permutated_matrix, max_rank=4, min_singular_val=0)
-    compressed_permutated_matrix.draw_matrix(f"./zad4/data/k_{k}/2d.png")
+    compressed_permutated_matrix.draw_matrix(f"./zad4/data/k_{k}/2d-min_deg.png")
 
+
+
+    # Reversed-Cuthil-Mckee
+    #draw matrix after permutation
+    permutation_engine = PermutationEngine('reversed_cuthill_mckee')
+
+    permutated_matrix = permutation_engine.permutate(matrix.copy())
+    sparse_draw(permutated_matrix, f"./zad4/data/k_{k}/2c-rev_cut_mc.png")
+    
+
+    #draw matrix after compressiona and permutation
+    compressed_permutated_matrix = compress_matrix(permutated_matrix, max_rank=4, min_singular_val=0)
+    compressed_permutated_matrix.draw_matrix(f"./zad4/data/k_{k}/2d-rev_cut_mc.png")
+
+
+    # Cuthil-Mckee
+    #draw matrix after permutation
+    permutation_engine = PermutationEngine('cuthill_mckee')
+
+    permutated_matrix = permutation_engine.permutate(matrix.copy())
+    sparse_draw(permutated_matrix, f"./zad4/data/k_{k}/2c-cut_mc.png")
+    
+
+    #draw matrix after compressiona and permutation
+    compressed_permutated_matrix = compress_matrix(permutated_matrix, max_rank=4, min_singular_val=0)
+    compressed_permutated_matrix.draw_matrix(f"./zad4/data/k_{k}/2d-cut_mc.png")
--- zad4/permutations.py
+++ zad4/permutations.py
@@ -2,79 +2,88 @@ import numpy as np
 import networkx as nx
 from collections import deque
 
-def minimum_degree_permutation(matrix):
-    n,m = matrix.shape
-    ADJ = {i:set() for i in range(n)}
-    for i in range(n):
-        for j in range(m):
-            if matrix[i][j] != 0 and i != j:
-                ADJ[i].add(j)
-    permutation = []
-    for i in range(n):
-        deg_min = m+1
-        for v, adj in ADJ.items():
-            if len(adj) < deg_min:
-                v_min = v
-                deg_min = len(adj)
-        for v in ADJ:
-            ADJ[v] = ADJ[v].difference([v_min])
-        for u in ADJ[v_min]:
-            ADJ[u] = (ADJ[u].union(ADJ[v_min].difference([u])))
-        ADJ.pop(v_min)
-        permutation.append(v_min)
-    return permutation
 
-def cuthill_mckee(matrix):
 
-    def BFS():
-        while Q:
-            v = Q.popleft()
-            if Visited[v]:
+class PermutationEngine:
+
+    def __init__(self, algorithm):
+        self.permutation_algorithm = {'minimum_degree': self._minimum_degree_permutation,
+                            'cuthill_mckee': self._cuthill_mckee,
+                            'reversed_cuthill_mckee': self._reversed_cuthill_mckee}.get(algorithm)
+
+
+    def _minimum_degree_permutation(self, matrix):
+        n,m = matrix.shape
+        ADJ = {i:set() for i in range(n)}
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] != 0 and i != j:
+                    ADJ[i].add(j)
+        permutation = []
+        for i in range(n):
+            deg_min = m+1
+            for v, adj in ADJ.items():
+                if len(adj) < deg_min:
+                    v_min = v
+                    deg_min = len(adj)
+            for v in ADJ:
+                ADJ[v] = ADJ[v].difference([v_min])
+            for u in ADJ[v_min]:
+                ADJ[u] = (ADJ[u].union(ADJ[v_min].difference([u])))
+            ADJ.pop(v_min)
+            permutation.append(v_min)
+        return permutation
+
+    def _cuthill_mckee(self, matrix):
+
+        def BFS():
+            while Q:
+                v = Q.popleft()
+                if Visited[v]:
+                    continue
+                permutation.append(v)
+                Visited[v] = True
+                for u in sorted(nx.neighbors(G, v), key = lambda x : G.degree(x)):
+                    if not Visited[u]:
+                        Q.append(u)
+        n = len(matrix)
+        
+        G = nx.Graph()
+        G.add_nodes_from(range(n))
+        
+        for i in range(n):
+            for j in range(n):
+                if matrix[i][j] != 0 and i != j:
+                    G.add_edge(i, j)
+
+        permutation = []
+        Visited = [False for i in range(n)]
+        sorted_nodes = sorted([x for x in G.degree()], key = lambda x : x[1])
+        # print(sorted_nodes)
+        sorted_nodes = list(map(lambda x : x[0], sorted_nodes))
+        Q = deque()
+        for s in sorted_nodes:
+            if not Visited[s]:
+                Q.append(s)
+                BFS()
+        
+        return permutation
+
+    def _reversed_cuthill_mckee(self, matrix):
+        return self._cuthill_mckee(matrix)[::-1]
+
+    def permutate(self, matrix):
+        permutation = self.permutation_algorithm(matrix)
+        new_matrix = matrix.copy()
+        for i in range(len(permutation)):
+            if i == permutation[i]:
                 continue
-            permutation.append(v)
-            Visited[v] = True
-            for u in sorted(nx.neighbors(G, v), key = lambda x : G.degree(x)):
-                if not Visited[u]:
-                    Q.append(u)
-    n = len(matrix)
-    
-    G = nx.Graph()
-    G.add_nodes_from(range(n))
-    
-    for i in range(n):
-        for j in range(n):
-            if matrix[i][j] != 0 and i != j:
-                G.add_edge(i, j)
+            new_matrix[i,:] = matrix[permutation[i],:].copy()
 
-    permutation = []
-    Visited = [False for i in range(n)]
-    sorted_nodes = sorted([x for x in G.degree()], key = lambda x : x[1])
-    # print(sorted_nodes)
-    sorted_nodes = list(map(lambda x : x[0], sorted_nodes))
-    Q = deque()
-    for s in sorted_nodes:
-        if not Visited[s]:
-            Q.append(s)
-            BFS()
+        matrix = new_matrix.copy()
+        for i in range(len(permutation)):
+            if i == permutation[i]:
+                continue
+            new_matrix[:,i] = matrix[:,permutation[i]].copy()
 
-    
-    
-    return permutation
-
-def reversed_cuthill_mckee(matrix):
-    return cuthill_mckee(matrix)[::-1]
-
-def permutate(matrix, permutation):
-    new_matrix = matrix.copy()
-    for i in range(len(permutation)):
-        if i == permutation[i]:
-            continue
-        new_matrix[i,:] = matrix[permutation[i],:].copy()
-
-    matrix = new_matrix.copy()
-    for i in range(len(permutation)):
-        if i == permutation[i]:
-            continue
-        new_matrix[:,i] = matrix[:,permutation[i]].copy()
-
-    return new_matrix
+        return new_matrix
